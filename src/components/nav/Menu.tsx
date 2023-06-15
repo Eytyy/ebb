c2bc15198e23240cfde2f2f0cd9a8236ba619b36
eytyy <e.tayyem@gmail.com>
2023-06-15 21:19:53 +0300

    fix menu styling

--- src/components/nav/Menu.tsx
+++ src/components/nav/Menu.tsx
@@ -34,7 +34,7 @@ export default function Menu() {
 
   return (
     <motion.div
-      className="fixed bottom-16 left-20 right-20 top-16 z-10 grid grid-cols-2 bg-background"
+      className="fixed bottom-0 left-0 z-10 grid h-full w-full grid-cols-2 bg-background px-20 py-16"
       variants={nav_variants}
       initial="initial"
       animate="animate"
